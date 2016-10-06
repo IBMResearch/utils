202340b0a89e2eac4175043c275fc2f290e92784
Jesús Pérez <jesusprubio@gmail.com>
2016-10-06 09:41:55 +0200

    Hi world!

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # utils
-Some common utils we use in our projects: Lodash, Bluebird, Bluemix logging/error reporting, debug, etc.
+
+[![Continuos integration](https://api.travis-ci.org/IBMResearch/utils.svg)](https://travis-ci.org/IBMResearch/utils)
+
+Some common utils we use in our projects: Lodash, Bluebird, Bluemix logging/error reporting, debug ...
+
+
+## Install
+
+We're not pushing it to npm because it's
+
+```sh
+npm i --save IBMResearch/utils
+```
+
+
+## Developer guide
+
+- Use [GitHub pull requests](https://help.github.com/articles/using-pull-requests).
+- We love ES6, so we use [ESLint](http://eslint.org/) and the [Airbnb](https://github.com/airbnb/javascript) style guide. It's the most complete, so it forces the developers to keep a consistent style.
+- Please run to be sure your code fits with it and the tests keep passing:
+```sh
+npm test
+```
+
+
+### Commit messages rules:
+- It should be formed by a one-line subject, followed by one line of white space. Followed by one or more descriptive paragraphs, each separated by one￼￼￼￼ line of white space. All of them finished by a dot.
+- If it fixes an issue, it should include a reference to the issue ID in the first line of the commit.
+- It should provide enough information for a reviewer to understand the changes and their relation to the rest of the code.
---
+++ index.js
@@ -0,0 +1,74 @@
+/*
+  MIT License
+
+  Copyright (c) 2016 IBM Research Emergent Solutions
+                     Jesús Pérez <jesusprubio@gmail.com>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+'use strict';
+
+const debug = require('debug');
+// Lodash as base.
+const utils = require('lodash');
+
+const pathToTag = require('./lib/pathToTag');
+
+
+// Attaching more common stuff from here.
+
+utils.debug = (projectName, fullPath) => debug(`${projectName}:${pathToTag(fullPath)}`);
+
+utils.Promise = require('bluebird');
+
+
+// From here some helpers to print in Bluemix native logs and Monitoring and
+// Analytics addon in a more or less comfortable way. Both don't support colors :(.
+
+// Not errors but we want them always printed.
+// Things we want to know that happened (ie: bad login).
+// Keep it to the minimal, because it's also printed in production.
+// and "console.*" are sync.
+utils.info = (msg, obj) => {
+  let finalMsg = `\n${msg || '-'}`;
+
+  if (obj) { finalMsg = `${finalMsg}: ${JSON.stringify(obj, null, 2)}`; }
+
+  console.log(finalMsg); // eslint-disable-line no-console
+};
+
+
+// Only for critical errors.
+utils.error = (msg, err) => {
+  const separator = '----------';
+  let finalMsg = `\nERROR: ${msg || '-'}`;
+
+  if (err) {
+    if (err.message) { finalMsg = `${finalMsg}\nmessage: ${err.message}`; }
+    if (err.stack) {
+      finalMsg = `${finalMsg}\nstack:\n${separator}\n${err.stack}\n${separator}`;
+    }
+  }
+
+  console.error(finalMsg); // eslint-disable-line no-console
+};
+
+
+module.exports = utils;
